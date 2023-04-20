89cd6cb54d887de61aa3b8a75596873e29ea28a7
Jacob <coneter@mail.ru>
2023-04-20 19:41:10 +0300

    fix audio paths

--- project_X/js/bundle.js
+++ project_X/js/bundle.js
@@ -1109,59 +1109,59 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "setAudioToHero": () => (/* binding */ setAudioToHero)
 /* harmony export */ });
 // Main
-const AudioBackground = new Audio("../../audio/background.mp3");
+const AudioBackground = new Audio("../audio/background.mp3");
 decVolume(AudioBackground, 0.2);
-const AudioClickSliderArrows = new Audio("../../audio/click_slider-arrows.mp3");
+const AudioClickSliderArrows = new Audio("../audio/click_slider-arrows.mp3");
 decVolume(AudioClickSliderArrows, 0.5);
-const AudioHeroChosen = new Audio("../../audio/hero_chosen.mp3");
+const AudioHeroChosen = new Audio("../audio/hero_chosen.mp3");
 decVolume(AudioHeroChosen, 0.3);
 // sword Strikes
-const AudioSwordStrike = new Audio("../../audio/sword_strike.mp3");
+const AudioSwordStrike = new Audio("../audio/sword_strike.mp3");
 decVolume(AudioSwordStrike, 0.1);
-const AudioSwordStrike2 = new Audio("../../audio/sword_strike_2.mp3");
+const AudioSwordStrike2 = new Audio("../audio/sword_strike_2.mp3");
 decVolume(AudioSwordStrike2, 0.1);
-const AudioSwordStrike3 = new Audio("../../audio/sword_strike_3.mp3");
+const AudioSwordStrike3 = new Audio("../audio/sword_strike_3.mp3");
 decVolume(AudioSwordStrike3, 0.1);
-const AudioSwordCrit = new Audio("../../audio/sword_strike_crit.mp3");
+const AudioSwordCrit = new Audio("../audio/sword_strike_crit.mp3");
 decVolume(AudioSwordCrit, 0.2);
 // punch Strikes
-const AudioPunchStrike = new Audio("../../audio/punch_strike.mp3");
+const AudioPunchStrike = new Audio("../audio/punch_strike.mp3");
 decVolume(AudioPunchStrike, 0.1);
-const AudioPunchStrike2 = new Audio("../../audio/punch_strike_2.mp3");
+const AudioPunchStrike2 = new Audio("../audio/punch_strike_2.mp3");
 decVolume(AudioPunchStrike2, 0.1);
-const AudioPunchStrike3 = new Audio("../../audio/punch_strike_3.mp3");
+const AudioPunchStrike3 = new Audio("../audio/punch_strike_3.mp3");
 decVolume(AudioPunchStrike3, 0.1);
-const AudioPunchCrit = new Audio("../../audio/punch_strike_crit.mp3");
+const AudioPunchCrit = new Audio("../audio/punch_strike_crit.mp3");
 decVolume(AudioPunchCrit, 0.2);
 // staff Strikes
-const AudioStaffStrike = new Audio("../../audio/staff_strike.mp3");
+const AudioStaffStrike = new Audio("../audio/staff_strike.mp3");
 decVolume(AudioStaffStrike, 0.1);
-const AudioStaffStrike2 = new Audio("../../audio/staff_strike_2.mp3");
+const AudioStaffStrike2 = new Audio("../audio/staff_strike_2.mp3");
 decVolume(AudioStaffStrike2, 0.1);
-const AudioStaffStrike3 = new Audio("../../audio/staff_strike_3.mp3");
+const AudioStaffStrike3 = new Audio("../audio/staff_strike_3.mp3");
 decVolume(AudioStaffStrike3, 0.1);
-const AudioStaffCrit = new Audio("../../audio/staff_strike_crit.mp3");
+const AudioStaffCrit = new Audio("../audio/staff_strike_crit.mp3");
 decVolume(AudioStaffCrit, 0.2);
 // Miss Attack
-const AudioMissAttack = new Audio("../../audio/miss_attack.mp3");
+const AudioMissAttack = new Audio("../audio/miss_attack.mp3");
 decVolume(AudioMissAttack, 0.2);
 // get Demage Man
-const AudioGetDemageMan = new Audio("../../audio/get_demage_man.mp3");
+const AudioGetDemageMan = new Audio("../audio/get_demage_man.mp3");
 decVolume(AudioGetDemageMan, 0.1);
-const AudioGetDemageMan2 = new Audio("../../audio/get_demage_man_2.mp3");
+const AudioGetDemageMan2 = new Audio("../audio/get_demage_man_2.mp3");
 decVolume(AudioGetDemageMan2, 0.1);
-const AudioGetDemageMan3 = new Audio("../../audio/get_demage_man_3.mp3");
+const AudioGetDemageMan3 = new Audio("../audio/get_demage_man_3.mp3");
 decVolume(AudioGetDemageMan3, 0.1);
-const AudioGetDemageMan4 = new Audio("../../audio/get_demage_man_4.mp3");
+const AudioGetDemageMan4 = new Audio("../audio/get_demage_man_4.mp3");
 decVolume(AudioGetDemageMan4, 0.1);
 // get Demage Woman
-const AudioGetDemageWoman = new Audio("../../audio/get_demage_woman.mp3");
+const AudioGetDemageWoman = new Audio("../audio/get_demage_woman.mp3");
 decVolume(AudioGetDemageWoman, 0.1);
-const AudioGetDemageWoman2 = new Audio("../../audio/get_demage_woman_2.mp3");
+const AudioGetDemageWoman2 = new Audio("../audio/get_demage_woman_2.mp3");
 decVolume(AudioGetDemageWoman2, 0.1);
-const AudioGetDemageWoman3 = new Audio("../../audio/get_demage_woman_3.mp3");
+const AudioGetDemageWoman3 = new Audio("../audio/get_demage_woman_3.mp3");
 decVolume(AudioGetDemageWoman3, 0.1);
-const AudioGetDemageWoman4 = new Audio("../../audio/get_demage_woman_4.mp3");
+const AudioGetDemageWoman4 = new Audio("../audio/get_demage_woman_4.mp3");
 decVolume(AudioGetDemageWoman4, 0.1);
 
 function decVolume(audio, volume) {
